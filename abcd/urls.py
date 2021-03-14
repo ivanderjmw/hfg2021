e23384b28c8de0c2bd540a1af0a41166053b5173
Ivander <ivanderjmw@gmail.com>
2021-03-14 18:14:31 +0800

    Create simple static layout

--- abcd/urls.py
+++ abcd/urls.py
@@ -3,5 +3,6 @@ from django.urls.conf import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.landing, name='landing')  # if matches raw empty string
+    re_path(r'^$', views.landing, name='landing'),  # if matches raw empty string
+    re_path(r'^abcd$', views.home, name='home')
 ]
